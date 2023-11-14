7d91caed92cb4cdf9a31ff7c535219b1a97fb8ca
JazzieL Rodríguez López <jazzielrodriguezlopez@gmail.com>
2023-11-13 22:40:45 -0600

    Agregando fizzbuzz

--- Fizzbuzz/fizzbuzz.js
+++ Fizzbuzz/fizzbuzz.js
@@ -9,16 +9,12 @@ for (let i = 1; i <= 100; i++) {
     else {
         if ((i % 3) === 0 && (i % 5 != 0)) {
             console.log("fizz")
-            
         }
         else if ((i % 5) == 0 && (i % 3 != 0)) {
-            console.log("buzz")
-            
+            console.log("buzz") 
         }
         else {
             console.log(i)
         }
-    
-
     }
 }
