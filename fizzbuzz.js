c5d27572d7685b0e335d58abe31232d721280a32
JazzieL Rodríguez López <jazzielrodriguezlopez@gmail.com>
2023-11-13 22:37:32 -0600

    Agregando ejercicio fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,24 @@
+/*
+    Programa que imprime los numeros del 1 al 100, los multiplos de 3 los cambie por la palabra fizz, los multiplos de 5 que cambie por buzz y los que compartan ser multiplos de 3 y 5 al mismo tiempo cambiar por fizzbuzz
+*/
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("fizzbuzz")
+    }
+    else {
+        if ((i % 3) === 0 && (i % 5 != 0)) {
+            console.log("fizz")
+            
+        }
+        else if ((i % 5) == 0 && (i % 3 != 0)) {
+            console.log("buzz")
+            
+        }
+        else {
+            console.log(i)
+        }
+    
+
+    }
+}
